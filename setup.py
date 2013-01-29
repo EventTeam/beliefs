c821fba9db90774687464127ff56ac95b62102d6
Dustin Smith <dustin@media.mit.edu>
2013-01-29 04:08:28 -0500

    added colormath to dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='beliefs',
       version='0.1',
-      install_requires=['distribute'],
+      install_requires=['distribute', 'colormath'],
       description=['Partial-information datastructures used to represent belief states'],
       url="https://github.com/EventTeam/beliefs",
       author='Dustin Smith',
